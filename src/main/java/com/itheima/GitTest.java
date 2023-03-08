e5672b5c26197ffebb65f421017c2ba07e8281cd
zy <2550267152@qq.com>
2023-03-08 09:08:54 +0800

    push commit

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello Git3");
         System.out.println("hello Git4");
         System.out.println("master test");
-        System.out.println("hot-fix demo");
+        System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
