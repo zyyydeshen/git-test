d0e91286cc100ec8ed20337c3fa6bfeac420b5d7
zy <2550267152@qq.com>
2023-03-08 08:40:27 +0800

    hot-fix first

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git2");
         System.out.println("hello Git3");
+        System.out.println("hello Git4");
     }
 }
