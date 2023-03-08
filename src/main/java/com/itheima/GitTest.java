f943a0b48c9448b1aa0fa92a8ed99b26c221e650
zy <2550267152@qq.com>
2023-03-08 08:47:03 +0800

    master commit

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello Git2");
         System.out.println("hello Git3");
         System.out.println("hello Git4");
+        System.out.println("master test");
     }
 }
